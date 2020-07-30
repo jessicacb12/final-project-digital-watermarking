Softmax hardcode fix

watermarking/cnn.py
@@ -18,7 +18,8 @@ from numpy import (
 # from numpy import max as max_from_array
 from numpy import sum as sum_array
 from scipy.signal import convolve2d
-from watermarking import value_with_position
+from watermarking import value_with_position, forward
+import tensorflow as tf
 
 class CNN:
     """Keeps atrribute embedding map and weight"""
@@ -37,11 +38,14 @@ class CNN:
             # [3, 512]
         ],
         "dec": [
-            [2, 1, [8, 1]],
+            [2, 8, [8, 8]],
             #[2, 4],
             # [3, 128],
             # [3, 256],
             # [3, 512]
+        ],
+        "softmax": [
+            ['fg', 'bg'], 8
         ]
     }
     ENCODER = "enc"
@@ -149,37 +153,33 @@ class CNN:
     def init_batch_norm():
         """Initialize batch norms gamma beta."""
         batch_norm_params = {}
+        params_list = ['gamma', 'beta', 'average', 'variance']
         for part in [CNN.ENCODER, CNN.DECODER]:
             side = CNN.INPUT_SIZE
             for i in range(0, len(CNN.CONVOLUTION_ORDERS[part])):
-                print(part, ' ', i, ' -> ', side)
-                batch_norm_params[
-                    part + "-" + str(i) + "-gamma"
-                ] = CNN.init_single_batch_norm_param(
-                    part + "-" + str(i),
-                    "gamma",
-                    side
-                )
-                batch_norm_params[
-                    part + "-" + str(i) + "-beta"
-                ] = CNN.init_single_batch_norm_param(
-                    part + "-" + str(i),
-                    "beta",
-                    side
-                )
-                side //= 2
+                for param_name in params_list:
+                    batch_norm_params[
+                        part + str(i) + "-" + param_name
+                    ] = CNN.init_single_batch_norm_param(
+                        part + str(i),
+                        param_name,
+                        CNN.CONVOLUTION_ORDERS[part][i][1] # based on number of channel output
+                    )
 
         return batch_norm_params
 
     @staticmethod
     def init_softmax_kernels():
         """Initialize kernels for softmax."""
-        kernels = {}
-        name, foreground = CNN.init_kernels(kernel_name="softmax-fg")
-        kernels[name] = foreground
-        name, background = CNN.init_kernels(kernel_name="softmax-bg")
-        kernels[name] = background
-        return kernels
+        kernels = []
+        for i, part in enumerate(CNN.CONVOLUTION_ORDERS['softmax'][0]):
+            kernels.append([])
+            for _input in range(CNN.CONVOLUTION_ORDERS['softmax'][1]):
+                _, kernel = CNN.init_kernels(
+                    kernel_name='softmax-' + part + str(_input)
+                )
+                kernels[i].append(kernel)
+        return array(kernels)
 
     @staticmethod
     def store_kernel(file, rows):
@@ -223,11 +223,12 @@ class CNN:
         try:
             param = CNN.read_kernel(param_structure + "-" + param_name)
         except FileNotFoundError:
-            param = zeros(
-                (row_length, row_length)
-            ) if param_name == 'beta' else ones(
-                (row_length, row_length)
-            )
+            if param_name == 'beta':
+                param = zeros((row_length)) 
+            elif param_name == 'gamma':
+                param = ones((row_length))
+            else:
+                param = None
         return param
 
     # Forward area
@@ -241,18 +242,45 @@ class CNN:
         # tf.constant(np.ones(len(a)), dtype=tf.float32),
         # 0.001
     #)
+
     @staticmethod
-    def batch_norm(matrices, beta, gamma, epsilon=0.1):
+    def expand_batch_norm_param(param):
+        """
+        Since this keep on becoming problem, each param seems has to be 
+        expanded into shape (batch,channel,1,1)
+        """
+        param = array(param)
+        return param.reshape(
+            param.shape[0], param.shape[1], 1, 1
+        )
+
+    @staticmethod
+    def batch_norm(
+            matrices,
+            beta,
+            gamma,
+            average=None,
+            variance=None,
+            epsilon=0.001
+        ):
         """Calculate batch normalization from matrices in a batch"""
         matrices = array(matrices)
-        average = mean(matrices, axis=0)
-        variance = var(matrices, axis=0)
+        beta = CNN.expand_batch_norm_param(beta)
+        gamma = CNN.expand_batch_norm_param(gamma)
+        if average == None:
+            average = mean(matrices, axis=1)
+        else:
+            average = CNN.expand_batch_norm_param(average)
+        if variance == None:
+            variance = var(matrices, axis=1)
+        else:
+            variance = CNN.expand_batch_norm_param(variance)
 
         number_mean = matrices - average
         normalized = number_mean / (sqrt(variance + epsilon))
-        # scaled_shift_data = normalized * gamma + beta
-        scaled_shift_data = normalized
-        print('norm: ', array(gamma).shape, ' ssd ', array(beta).shape)
+        scaled_shift_data = normalized * gamma + beta
+        # scaled_shift_data = normalized
+        print('norm: ', gamma.shape, ' ssd ', beta.shape)
         return scaled_shift_data, (
             normalized, number_mean, sqrt(variance + epsilon)
         )
@@ -318,19 +346,37 @@ class CNN:
 
     @staticmethod
     def trainable_softmax(kernels, matrix):
-        """Return classified matrix"""
-        foreground = convolve2d(
-            matrix,
-            flip(kernels["softmax-fg"]),
-            mode='same'
-        )
-        background = convolve2d(
-            matrix,
-            flip(kernels["softmax-bg"]),
-            mode='same'
+        """
+        Return classified matrix.
+        - Kernels shape will be 77(input number)2
+        - Matrix shape will be batch, width, height, channel
+        """
+
+        CNN.softmax_kernel = kernels
+
+        matrix = forward.Forward.reverse_shape(
+            tf.keras.layers.Conv2D(
+                len(CNN.CONVOLUTION_ORDERS['softmax'][0]),
+                CNN.CONVOLUTION_KERNEL_SIZE,
+                input_shape=matrix.shape[1:],
+                padding = 'same',
+                kernel_initializer=CNN.get_softmax_kernel,
+                use_bias=False
+            )(matrix).numpy()[0]
         )
 
+        foreground = matrix[
+            CNN.CONVOLUTION_ORDERS['softmax'][0].index('fg')
+        ]
+        background = matrix[
+            CNN.CONVOLUTION_ORDERS['softmax'][0].index('bg')
+        ]
+
         return CNN.softmax([background, foreground]), [background, foreground]
+
+    @staticmethod
+    def get_softmax_kernel(shape, dtype=None):
+        return CNN.softmax_kernel
 
     # tensorflow tested: tf.nn.softmax(matrix)
     @staticmethod

watermarking/forward.py
@@ -26,7 +26,7 @@ class Forward:
     def __init__(self, istraining, inputs, params):
         self.istraining = istraining
         self.inputs = array(inputs)
-        self.prnt('', self.inputs.shape)
+        print(self.inputs.shape)
         (
             self.scale_shift,
             self.encoder_kernels,
@@ -50,7 +50,7 @@ class Forward:
         encoded = []
         # encoder
         print('encoder', flush=True)
-        self.prnt('shape: ', self.inputs.shape)
+        print('shape: ', self.inputs.shape)
         encoded = self.inputs
         for i in range(len(cnn.CNN.CONVOLUTION_ORDERS[cnn.CNN.ENCODER])):
             print('STACK:', i)
@@ -61,12 +61,11 @@ class Forward:
                         image, self.ENCODER, i, batch
                     )
                 )
-            if len(encoded) > 1: # if batch > 1
-                conved_images = self.batch_norm_per_stack(
-                    conved_images,
-                    cnn.CNN.ENCODER,
-                    i
-                )
+            conved_images = self.batch_norm_per_stack(
+                conved_images,
+                cnn.CNN.ENCODER,
+                i
+            )
             processed_conv = []
             print('ReLU and max pool')
             for batch, image in enumerate(conved_images):
@@ -77,7 +76,7 @@ class Forward:
         decoded = encoded
         #decoder
         print('decoder', flush=True)
-        self.prnt('shape: ', array(decoded).shape)
+        print('shape: ', array(decoded).shape)
         for i in range(
                 len(cnn.CNN.CONVOLUTION_ORDERS[cnn.CNN.DECODER]) - 1,
                 -1, -1
@@ -94,16 +93,13 @@ class Forward:
                     image, self.DECODER, i, batch
                 )
                 processed.append(image)
-            if len(processed) > 1: # if batch > 1
-                decoded = self.batch_norm_per_stack(
-                    processed,
-                    cnn.CNN.DECODER,
-                    i
-                )
-            else:
-                decoded = processed		
-            # decoded = processed	
-            self.prnt('decoded: ', array(decoded).shape)
+            decoded = self.batch_norm_per_stack(
+                processed,
+                cnn.CNN.DECODER,
+                i
+            )
+            # decoded = array(processed)
+            print(decoded.shape)
         if self.istraining:
             return self.softmax_per_batch(decoded), (
                 self.convolution_cache,
@@ -145,11 +141,6 @@ class Forward:
         for _ in range(training.Training.BATCH_SIZE):
             self.relu_cache.append([])
             self.max_pooling_cache.append([])
-
-    def prnt(self, message, shape):
-        if not self.istraining:
-            shape = (1, shape[1], shape[2], shape[3])
-        print(message, shape)
 
     # manually tested in jupyter
     def conv_per_stack(self, matrices, part, stack_number, batch_number):
@@ -200,7 +191,7 @@ class Forward:
                 use_bias=False
             )(matrices).numpy()
             print(matrices.shape)
-        return self.reverse_shape(matrices[0])
+        return Forward.reverse_shape(matrices[0])
 
     def take_from_dict(self, kernels, address):
         param = []
@@ -229,7 +220,8 @@ class Forward:
     def get_current_kernel(shape, dtype=None):
         return Forward.current_kernel
 
-    def reverse_shape(self, feature_map):
+    @staticmethod    
+    def reverse_shape(feature_map):
         """64, 64, 8 -> 8, 64, 64"""
         ch_number = feature_map.shape[2]
         new_fm = []
@@ -266,11 +258,13 @@ class Forward:
 
     def batch_norm_per_stack(self, matrices, part, stack_number):
         """Process each batch member with Batch Normalization"""
-        self.prnt('BN', array(matrices).shape)
+        matrices = array(matrices)
         normalized, cache = cnn.CNN.batch_norm(
             matrices,
-            self.scale_shift[part + "-" + str(stack_number) + "-beta"],
-            self.scale_shift[part+ "-" + str(stack_number) + '-gamma']
+            self.scale_shift[part + str(stack_number) + "-beta"],
+            self.scale_shift[part + str(stack_number) + '-gamma'],
+            average=self.scale_shift[part + str(stack_number) + "-average"],
+            variance=self.scale_shift[part + str(stack_number) + "-variance"]
         )
         if self.istraining:
             self.batch_norm_cache.append(cache)
@@ -303,7 +297,7 @@ class Forward:
         upsampled = tf.keras.layers.UpSampling2D()(
             array([self.fix_reverse_shape_3d(matrices)])
         ).numpy()
-        return self.reverse_shape(upsampled[0])
+        return Forward.reverse_shape(upsampled[0])
 
     def softmax_per_batch(self, matrices):
         """Process each batch or single matrix into softmax output(s)"""
@@ -314,8 +308,7 @@ class Forward:
                 self.conv_softmax_cache.append(matrix)
                 result, cache = cnn.CNN.trainable_softmax(
                     self.softmax_kernels,
-                    matrix[0] # has to access 0 because there seems
-                    # to be an extra channel dimension
+                    matrix
                 )
                 softmax_per_batch.append(result)
                 self.softmax_cache.append(cache)
@@ -323,9 +316,11 @@ class Forward:
             return softmax_per_batch
         else:
             (foreground, background), _ = cnn.CNN.trainable_softmax(
-                self.softmax_kernels,
-                matrices[0][0] # has to 0 0 because there will be
-                # dimension for batch and channel
+                self.fix_reverse_shape_4d(
+                    self.softmax_kernels
+                ),
+                array([self.fix_reverse_shape_3d(matrices[0])]) 
+                # 0 because batch dimension will be there
             )
             result = cnn.CNN.softmax_classifier(
                 foreground, background